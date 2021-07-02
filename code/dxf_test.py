3eec5f008ad9afb247c4fcc69811102281931974
briansune <briansune@gmail.com>
2021-07-03 00:56:14 +0800

    update

--- code/dxf_test.py
+++ code/dxf_test.py
@@ -3,6 +3,8 @@ import sys
 from pprint import pprint
 import numpy as np
 
+coord_dict = {}
+
 try:
     dwg = ezdxf.readfile(r"D:\orcad_learn\test\ttttt.dxf")
     # dwg = ezdxf.readfile(r"D:\orcad_learn\test\test_dxf.dxf")
@@ -13,14 +15,29 @@ try:
     layer_names = [layer.dxf.name for layer in dwg.layers]
     print(layer_names)
 
-    coord_dict = {}
+    plyll_n = list(msp.query('LWPOLYLINE'))
+    plyll_o = list(msp.query('POLYLINE'))
+    if plyll_n:
+        plyll = plyll_n
+    elif plyll_o:
+        plyll = plyll_o
+    else:
+        plyll = []
 
-    for polyline in msp.query('LWPOLYLINE'):
+    for polyline in plyll:
         key = polyline.get_dxf_attrib('handle')
         tmp = []
         bulge_flag = False
         bulge_1st_coord = []
-        for coord in polyline.get_points():
+
+        try:
+            points_list = polyline.get_points()
+        except AttributeError:
+            points_list = []
+            for d1, d2 in zip(polyline.points(), polyline.vertices()):
+                points_list.append(list(d1 + tuple([d2.dxf.bulge])))
+
+        for coord in points_list:
             tmp.append([coord[0], coord[1], coord[-1]])
             if bulge_flag:
                 bulge_flag = False
@@ -35,29 +52,6 @@ try:
                 tmp.append([cx, cy, 0.0])
             if coord[-1] != 0:
                 bulge_1st_coord = [coord[0], coord[1], coord[-1]]
-                bulge_flag = True
-        coord_dict[key] = tmp
-
-    for polyline in msp.query('POLYLINE'):
-        key = polyline.get_dxf_attrib('handle')
-        tmp = []
-        bulge_flag = False
-        bulge_1st_coord = []
-        for coord, bulge in zip(polyline.points(), polyline.vertices()):
-            tmp.append(list(coord + tuple([bulge.dxf.bulge])))
-            if bulge_flag:
-                bulge_flag = False
-                mid_pt = [(bulge_1st_coord[0] + coord[0]) / 2,
-                          (bulge_1st_coord[1] + coord[1]) / 2]
-                radius = np.sqrt((coord[0] - bulge_1st_coord[0]) ** 2 +
-                                 (coord[1] - bulge_1st_coord[1]) ** 2) / 2
-                angle = np.arctan2(coord[1] - mid_pt[1],
-                                   coord[0] - mid_pt[0]) + np.pi / 2
-                cx = mid_pt[0] - radius * np.cos(angle) * bulge_1st_coord[-1]
-                cy = mid_pt[1] - radius * np.sin(angle) * bulge_1st_coord[-1]
-                tmp.append([cx, cy, 0.0])
-            if bulge.dxf.bulge != 0:
-                bulge_1st_coord = [coord[0], coord[1], bulge.dxf.bulge]
                 bulge_flag = True
         coord_dict[key] = tmp
 
